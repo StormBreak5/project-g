450b0f6789930afd26fa228b80cedc6d43abc9cc
Itzac <itzacalbertin@gmail.com>
2026-01-02 11:07:42 -0300

    Estrutura inicial

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+import socketio
+from fastapi import FastAPI
+
+#Servidor
+sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
+app = FastAPI()
+socket_app = socketio.ASGIApp(sio, app)
+
+@sio.event
+async def connect(sid, environ):
+    print(f"Cliente conectado: {sid}")
+    await sio.emit('message', {'message': 'Conectado ao Python com sucesso !'}, to=sid)
+
+@sio.event
+async def ping_server(sid, data):
+    print(f"Ping recebido: {data}")
+    await sio.emit('pong_server', {'message': 'Pong de volta !'}, to=sid)
